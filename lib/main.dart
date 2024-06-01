e5972f919511da71e1f6658e693d6e93876b25f5
jksan <jksan.jp@gmail.com>
2024-06-01 10:46:18 +0900

    イメージから顔検出

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
@@ -36,6 +37,29 @@ class _CameraAppState extends State<CameraApp> {
       ResolutionPreset.high,
     );
     _initializeControllerFuture = _controller.initialize();
+
+    _loadImage();
+  }
+
+  Future<void> _loadImage() async {
+    final File f = await getImageFileFromAssets('IMG_3201.PNG');
+    print(f.path);
+
+    final inputImage = InputImage.fromFilePath(f.path);
+    final faceDetector = GoogleMlKit.vision.faceDetector();
+    final faces = await faceDetector.processImage(inputImage);
+    print('検出された顔の数: ${faces.length}\n\n');
+  }
+
+  Future<File> getImageFileFromAssets(String path) async {
+    final byteData = await rootBundle.load('assets/$path');
+
+    final file =
+        File('${(await getApplicationDocumentsDirectory()).path}/$path');
+    await file.writeAsBytes(byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+    return file;
   }
 
   @override
@@ -65,6 +89,7 @@ class _CameraAppState extends State<CameraApp> {
             final image = await _controller.takePicture();
 
             final imageFile = File(image.path);
+            print(image.path);
             final inputImage = InputImage.fromFile(imageFile);
 
             final faceDetector = GoogleMlKit.vision.faceDetector();
